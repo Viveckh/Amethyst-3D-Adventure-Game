4889e1197ccae108a89e724ad5518881798d6059
adithyat <adithya35@gatech.edu>
2022-03-10 14:15:25 -0500

    Created Kyle the Killer

---
+++ Amethyst/Assets/Prefabs/Robot Kyle/Scripts/KyleKiller.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class KyleKiller : MonoBehaviour
+{
+
+    
+    public Transform target;
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent=GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+        FaceTarget();
+    }
+    void FaceTarget(){
+        Vector3 direction = (target.position- transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x,0,direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation,Time.deltaTime*5f);
+    }
+}
