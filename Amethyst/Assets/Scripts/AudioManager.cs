10305fb1226e06cd028f9319a4e0c4d42e85b655
Vivek Pandey (EJ, Vek) <vpandey@ramapo.edu>
2022-03-13 18:27:30 -0400

    audio is in

--- Amethyst/Assets/Scripts/AudioManager.cs
+++ Amethyst/Assets/Scripts/AudioManager.cs
@@ -6,21 +6,38 @@ public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
 
+    public GameObject riverAudioPoint;
+    public GameObject warehouseAudioPoint;
+
     void Awake()
     {
         foreach (Sound s in sounds)
         {
-            s.source = gameObject.AddComponent<AudioSource>();
+            if (s.name == "river") {
+                s.source = riverAudioPoint.AddComponent<AudioSource>();
+            }
+            else if (s.name == "warehouse") {
+                s.source = warehouseAudioPoint.AddComponent<AudioSource>();
+            }
+            else
+            {
+                // Attach the sound to audio manager by default
+                s.source = gameObject.AddComponent<AudioSource>();
+            }
             s.source.clip = s.clip;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
+            s.source.spatialBlend = s.spatialBlend;
         }
     }
 
     void Start()
     {
-        Play("theme");
+        // Play the theme
+        // Play("theme");
+        Play("river"); // you can only hear when close to the river
+        Play("warehouse"); // you can only hear when close to the warehouse
     }
 
     public void Play(string name)
--- Amethyst/Assets/Scripts/Sound.cs
+++ Amethyst/Assets/Scripts/Sound.cs
@@ -16,6 +16,9 @@ public class Sound
 
     public bool loop;
 
+    [Range(0f, 1f)]
+    public float spatialBlend;
+
     [HideInInspector]
     public AudioSource source;
 }
